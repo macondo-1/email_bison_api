566642b5c58a8aa6ce4be7b6a8f6e4423b3f8c15
alberto <sleeps.doyens0y@icloud.com>
2025-07-03 18:12:16 -0600

    created more functions to make api calls

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import credentials as cred
+import constants as const
 import http.client
 import json
 import csv
+import re
 
 def make_api_call(method, api_call, payload):
     base_link = 'mail.sisconsult.co'
@@ -33,16 +35,19 @@ def list_campaigns():
     'archived'
     print('Available status:\n', available_status)
     status = input('Select 1 status: ')
-    #payload = "{\"search\": null,\n  \"status\": \"active\"}"
-
-    #MODIFY: THIS IS NOT WORKING
     payload = {
         'status':'{}'.format(status)
     }
     payload = json.dumps(payload).encode('utf-8')
     api_call = '/api/campaigns'
     method = 'GET'
-    make_api_call(method, api_call, payload)
+    records = make_api_call(method, api_call, payload)
+    records = json.loads(records)
+    with open('campaigns_info.csv', 'w') as file:
+        fieldnames = records['data'][0].keys()
+        writer = csv.DictWriter(file, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerows(records['data'])
 
 def create_a_campaign(campaign_name:str):
     payload = {
@@ -127,39 +132,149 @@ def bulk_create_leads(filename):
     count = 0
     records_dict = {}
     records_dict['leads'] = a
+    remaining_dict = {}
 
-    payload = records_dict
-    for x in payload['leads']:
-        x['last_name'] = 'None'
-    payload = json.dumps(payload).encode('utf-8')
-    method = 'POST'
-    api_call = '/api/leads/multiple'
-    data = make_api_call(method, api_call, payload)
+    if len(records_dict['leads']) > 500:
+        while len(records_dict['leads']) > 500:
+            records_dict['leads'] = records_dict['leads'][:500]
+            remaining_dict['leads'] = records_dict['leads'][500:]
+            payload = records_dict
+            for x in payload['leads']:
+                x['last_name'] = 'None'
+            payload = json.dumps(payload).encode('utf-8')
+            method = 'POST'
+            api_call = '/api/leads/multiple'
+            data = make_api_call(method, api_call, payload)
 
-    leads_dict = json.loads(data)
+            leads_dict = json.loads(data)
 
-    if leads_dict['errors']:
-        delete_index = []
-        for x in leads_dict['errors'].keys():
-            delete_index.append(int(x.split('.')[1]))
+            if 'errors' in leads_dict.keys(): #modify: figure out why it fails here?
+                delete_index = []
+                for x in leads_dict['errors'].keys():
+                    delete_index.append(int(x.split('.')[1]))
 
-        for x in delete_index:
-            del records_dict['leads'][x]
+                delete_index.reverse()
+                for x in delete_index:
+                    del records_dict['leads'][x]
 
+                payload = records_dict
+                payload = json.dumps(payload).encode('utf-8')
+                data = make_api_call(method, api_call, payload)
+
+            append_new_leads()
+            records_dict['leads'] = remaining_dict['leads']
+
+    else:
         payload = records_dict
+        for x in payload['leads']:
+            x['last_name'] = 'None'
         payload = json.dumps(payload).encode('utf-8')
+        method = 'POST'
+        api_call = '/api/leads/multiple'
         data = make_api_call(method, api_call, payload)
 
-    return data
+        leads_dict = json.loads(data)
+
+        if 'errors' in leads_dict.keys(): #modify: figure out why it fails here?
+            delete_index = []
+            for x in leads_dict['errors'].keys():
+                delete_index.append(int(x.split('.')[1]))
+
+            delete_index.reverse()
+            for x in delete_index:
+                del records_dict['leads'][x]
+
+            payload = records_dict
+            payload = json.dumps(payload).encode('utf-8')
+            data = make_api_call(method, api_call, payload)
+
+        append_new_leads()
+
+    # out of loop
+    ids_list = search_leads_ids(filename)
+
+    return ids_list
         
 def get_all_leads():
+    count = 0
     payload = ''
     method = 'GET'
     api_call = '/api/leads'
+  
     leads_list = make_api_call(method, api_call, payload)
+    leads_dict = json.loads(leads_list)
+    print(leads_dict)
+    
+    while leads_dict['links']['next']:
+        api_call = leads_dict['links']['next'].split('https://mail.sisconsult.co')[1]
+        leads_list = make_api_call(method, api_call, payload)
+        leads_dict_1 = json.loads(leads_list)
 
+        # Correctly merge new data
+        leads_dict['data'].extend(leads_dict_1['data'])
+
+        # Update the 'links' dict so we follow the correct next page
+        leads_dict['links'] = leads_dict_1.get('links', {})
+
+        count += 1
+
+        print(count)
+
+    with open('test.csv', 'w') as file:
+        fieldnames = leads_dict['data'][0].keys()
+        writer = csv.DictWriter(file, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerows(leads_dict['data'])
+        
     return leads_list
   
+def append_new_leads():
+
+    # read existing
+    existing_emails = []
+    records = []
+    with open('test.csv', 'r') as file:
+        reader = csv.DictReader(file)
+        for x in reader:
+            # get mails
+            existing_emails.append(x['email'])
+            # get records
+            records.append(x)
+
+    reached_existing_emails = False
+    records_to_append = []
+    count = 0
+    page_number = 1
+    while not reached_existing_emails:
+        # get 15 new records
+        
+        payload = ''
+        method = 'GET'
+        api_call = '/api/leads?page={}'.format(page_number)
+        print(api_call)
+        leads_list = make_api_call(method, api_call, payload)
+        leads_dict = json.loads(leads_list)
+
+        # records in mails?
+        
+        for x in leads_dict['data']:
+            if x['email'] in existing_emails: #this wont work
+                reached_existing_emails = True
+            else:
+                # add to records
+                records_to_append.append(x)
+        count += 1
+        page_number += 1
+        print(count)
+
+    records_to_append.extend(records)
+
+    with open('test.csv', 'w') as file:
+        fieldnames = records_to_append[0].keys()
+        writer = csv.DictWriter(file, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerows(records_to_append)
+
 def create_leads_list_from_csv(): # NOT WORKING
     import requests
 
@@ -267,53 +382,75 @@ def resume_campaign(campaign_id):
     payload=''
     make_api_call(method,api_call,payload)
 
+def create_new_project_in_email_bison():
+    campaign_name = '143386_sedivention_germany_20250703'
+    timezone = 'America/New_York'
+    mail_message_filename = '/Users/albertoruizcajiga/python/sis_international/files/projects/143386_sedivention/' \
+    '143386_sedivention.txt'
+    #list_file_name = 'Cayman_List_1_2_Filtered.csv'
 
-#  COMPLETE WORKFLOW
-# campaign_name = '124015_glassview_hearing_study_20250701'
-# max_emails_per_day = 1000
-# campaign_id = create_a_campaign(campaign_name)
-# update_campaign_settings(max_emails_per_day, campaign_id)
-# timezone = 'America/New_York'
-# create_campaign_schedule(campaign_id,timezone)
-# mail_message_filename = '/Users/albertoruizcajiga/python/sis_international/files/projects/124015_glassview_hearing_study/' \
-# '124015_glassview_hearing_study.txt'
-# create_sequence_steps(campaign_id, mail_message_filename)
-# import_sender_emails_by_id(campaign_id)
+    max_emails_per_day = 1000
+    campaign_id = create_a_campaign(campaign_name)
+    list_campaigns() # modify: check if this has worked
+    update_campaign_settings(max_emails_per_day, campaign_id)
+    create_campaign_schedule(campaign_id,timezone)
+    create_sequence_steps(campaign_id, mail_message_filename)
+    import_sender_emails_by_id(campaign_id)
+    # ids_list = bulk_create_leads(list_file_name)
+    # import_leads_by_id_to_campaign(campaign_id, ids_list)
+    # resume_campaign(campaign_id)
 
-campaign_id = 10
-# ids_list = []
-# with open('test.csv','r') as file:
-#     content = file.readlines()
-#     for x in content[1:]:
-#         ids_list.append(x.split(',')[0])
+def add_list_and_start_campaign():
+    campaign_id = int(input('Campaing id: '))
+    list_file_name = input('List name: ')
+    list_file_name = '{}.csv'.format(list_file_name)
+    ids_list = bulk_create_leads(list_file_name)
+    import_leads_by_id_to_campaign(campaign_id, ids_list)
+    resume_campaign(campaign_id)
 
-#working
-# leads_list = bulk_create_leads('usa_nyc_intern_entry_4.csv')
-# leads_dict = json.loads(leads_list)
-# ids_list = []
-# for x in leads_dict['data']:
-#     ids_list.append(x['id'])
-# import_leads_by_id_to_campaign(campaign_id, ids_list)
-#resume_campaign(campaign_id)
+def search_leads_ids(filename):
+    file_path = '/Users/albertoruizcajiga/Documents/Documents - Alberto’s MacBook Air/final_final/to_process/'
+    file_name = file_path + filename
+    a = []
+    with open(file_name, 'r') as file:
+        records = csv.DictReader(file)
+        for record in records:
+            a.append(record['email'])
+
+    ids_list = []
+    with open('test.csv', 'r') as file:
+        reader = csv.DictReader(file)
+        for x in reader:
+            if x['email'] in a:
+                ids_list.append(x['id'])
+
+    return ids_list
+
+def create_blacklisted_email():
+    filename = const.BLACKLIST_PATH
+    with open(filename, 'r') as file:
+        contents = file.read()
+        print(contents)
+
+    # payload = "{\"email\": \"john@doe.com\"}"
+
+    # method='POST'
+    # api_call = '/api/blacklisted-emails'
+    # payload=''
+    # make_api_call(method,api_call,payload)
+
+    # result = re.match(r'^([a-zA-Z0-9._%+-]+)@([a-zA-Z0-9.-]+)\.([a-zA-Z]{2,})$','christine.saenger@web.de')
+
+    # print(result.string)
+
+# TO-DO
+
+# bulk update email signature
+
+# add to block list - email addresses
+# add to block list - domains
 
 
 
-# checar
-# leads_list = bulk_create_leads('usa_nyc_intern_entry_2.csv')
-# leads_dict = json.loads(leads_list)
-# ids_list = []
-# for x in leads_dict['data']:
-#     ids_list.append(x['id'])
-
-# with open('test.csv', 'w') as file:
-#     fieldnames = leads_dict['data'][0].keys()
-#     writer = csv.DictWriter(file,fieldnames=fieldnames)
-#     writer.writeheader()
-#     writer.writerows(leads_dict['data'])
-
-leads_list = bulk_create_leads('usa_nyc_intern_entry_5.csv')
-leads_dict = json.loads(leads_list)
-
-# for x in leads_dict['errors'].keys():
-#     print(x.split('.')[1])
+add_list_and_start_campaign()
 
